9a249eda0257b0cc46f65684075043071f646334
Taumanni (Tsunami) <kiezagamer@gmail.com>
2025-09-14 19:40:00 -0300

    add read columns

--- EP1/EP1 - Analise.py
+++ EP1/EP1 - Analise.py
@@ -22,6 +22,7 @@ Created on Fri Sep 12 12:04:25 2025
 import pandas as pd
 import scipy as scp
 from scipy import stats
+from scipy.stats import norm, expon, lognorm, gamma, weibull_min
 import numpy as np
 import matplotlib.pyplot as plt
 import os 
@@ -33,3 +34,41 @@ df = pd.read_excel('Base de Dados.xlsx', sheet_name='Planilha3', skiprows=1)
 
 print(df.info())
 print(df.describe())
+
+df = df.rename(columns =
+    {
+        df.columns[9]: 'Data_Atracacao',
+        df.columns[10]: 'Data_Chegada',
+        df.columns[11]: 'Data_Desatracacao',
+        df.columns[12]: 'Data_Inicio_Operacao',
+        df.columns[13]: 'Data_Termino_Operacao'
+    }
+)
+
+# Funcionamento:
+# Chegada -> Espera -> Atraca -> Inicio Opera -> Opera -> Fim Opera -> Desatraca
+
+# Calculo dos tempos (em horas)
+df['Tempo_Espera'] = ( (df['Data_Inicio_Operacao'] - df['Data_Chegada']).dt.total_seconds() ) / 3600
+df['Tempo_Operacao'] = ( (df['Data_Termino_Operacao'] - df['Data_Inicio_Operacao']).dt.total_seconds() ) / 3600
+df['Tempo_Atracado'] = ( (df['Data_Desatracacao'] - df['Data_Atracacao']).dt.total_seconds() ) / 3600
+
+dados = df[['Tempo_Espera', 'Tempo_Operacao', 'Tempo_Atracado']].dropna()
+
+
+print("Dados de interesse")
+print(dados.describe())
+
+# Análise dos Outliers
+
+
+# Estatísticas básicas (média, variância, desvio padrão...)
+
+
+# Visualização dos dados
+
+
+# Testes de Aderência
+
+
+# Testes de distribuições estatísticas
